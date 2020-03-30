1fd3229706979fb678770ab2442fe9b828e6b21c
Jose Angel Espinoza Arce <jose.espinoza@banregio.com>
2020-03-30 15:48:14 -0600

    add featureml class

--- models.py
+++ models.py
@@ -19,6 +19,79 @@ replaces = [
 
 
 @dataclass
+class FeaturesBuro:
+    features: List[str]
+    campos: List[CampoAgregado] = field(init=False)
+
+    def __post_init__(self):
+        self.campos = self.construye_campos() 
+
+    def construye_campos(self):
+        for i in range(0, len(self.features)):
+            for rep in replaces:
+                if self.features[i] == rep:
+                    self.features[i] = rep.replace('_tu', '')
+        _campos = {}
+        with open('varmap.csv') as varmap_file:
+            for line in varmap_file:
+                line_split = line.strip('\n').split('|')
+                for feature in self.features:
+                    if line_split[0] == feature:
+                        try:
+                            field_split = line_split[1].split(',')
+                            subfield_split = field_split[2].split('-')
+                            if len(subfield_split) > 1:
+                                field_split[2] = CampoAgregado(*subfield_split)
+                            if field_split[-1] == '':
+                                field_split[-1] = None
+                            _campos[feature] = CampoAgregado(*field_split)
+                        except TypeError as e:
+                            print(e)
+                            raise e
+
+        return [_campos[feature] for feature in self.features]
+
+    @property
+    def campos_cuentas(self):
+        return tuple(filter(lambda x: x.segmento == 'cuentas', self.campos))
+
+    @property
+    def campos_consultas(self):
+        return tuple(filter(lambda x: x.segmento == 'consultas', self.campos))
+
+    @property
+    def subcampos_cuentas(self):
+        subcampos = set(map(
+            lambda x: x.campo,
+            filter(lambda x: not isinstance(x.campo, CampoAgregado), self.campos_cuentas)
+        ))
+        ret = set([])
+        while subcampos:
+            subcampo = subcampos.pop()
+            if subcampo.variable == 'mop':
+                ret.add(Campo('cuentas', 't_mop', subcampo.sufijo))
+                ret.add(Campo('cuentas', 'peso_mop', subcampo.sufijo))
+            else:
+                ret.add(subcampo)
+        return ret
+
+    @property
+    def subcampos_cuentas_ingresos(self):
+        return set(map(
+            lambda x: x.campo,
+            filter(lambda x: getattr(x.campo, 'segmento', None) == 'ingreso', self.campos_cuentas)
+        ))
+
+    @property
+    def subcampos_consultas(self):
+        return set(map(lambda x: x.campo, self.campos_consultas))
+
+    @property
+    def subcampos_score(self):
+        return set(map(lambda x: x.campo, self.campos_score))
+
+
+@dataclass
 class MLModel:
     xgb_model_path: str
     xgb_model: Booster = field(init=False)
